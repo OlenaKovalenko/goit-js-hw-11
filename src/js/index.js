da9f027a35f67ade214dcba3d6ddf5503c78f371
OlenaKovalenko <elenkow17@gmail.com>
2023-09-30 22:19:09 +0300

    Add refs

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './refs';
+
 
 const API_KEY = '39708192-1d0c61ff60ff411770af0a0fc';
+
+refs.searchForm.addEventListener('submit', onFormSubmit);
+
+async function onFormSubmit(event) {
+    event.preventDefault();
+
+    
+}
