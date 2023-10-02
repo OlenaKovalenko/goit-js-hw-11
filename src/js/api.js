ae784d5c5934264974915f6873ce8e1a0907da8c
OlenaKovalenko <131033072+OlenaKovalenko@users.noreply.github.com>
2023-10-02 09:55:36 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,37 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { page } from './index';
+
+export async function fetchBySearch(searchWord) {
+    try {
+    const API_KEY = '39708192-1d0c61ff60ff411770af0a0fc';
+    const BASE_URL = 'https://pixabay.com/api/';
+        
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: searchWord,
+        image_type: "photo",
+        orientation: "horizontale",
+        safesearch: true,
+        page: page,
+        per_page: 40,
+    });
+        
+    const response = await axios.get(`${BASE_URL}?${searchParams}`);
+    return response.data;
+  
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+
+// let searchWord = '';
 
 // export async function fetchBySearch(searchWord, page = 1) {
-//     try {
-//     const API_KEY = '39708192-1d0c61ff60ff411770af0a0fc';
+  
+//     const API_KEY = '39706325-fc42f933f9753c8c7490c2cf4';
 //     const BASE_URL = 'https://pixabay.com/api/';
         
 //     const searchParams = new URLSearchParams({
@@ -15,42 +43,16 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 //         per_page: 40,
 //     });
         
-//     const response = await axios.get(`${BASE_URL}?${searchParams}&page=${page}`);
-//     return response.data;
-  
-//   }
-//   catch (error) {
-//     throw error;
-//   }
-// }
+//   return await axios.get(`${BASE_URL}?${searchParams}&page=${page}`)
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => {
+//       Notify.failure('Error fetching cards info', error);
 
-
-let searchWord = '';
-
-export async function fetchBySearch(searchWord, page = 1) {
-  
-    const API_KEY = '39706325-fc42f933f9753c8c7490c2cf4';
-    const BASE_URL = 'https://pixabay.com/api/';
-        
-    const searchParams = new URLSearchParams({
-        key: API_KEY,
-        q: searchWord,
-        image_type: "photo",
-        orientation: "horizontale",
-        safesearch: true,
-        per_page: 40,
-    });
-        
-  return await axios.get(`${BASE_URL}?${searchParams}&page=${page}`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => {
-      Notify.failure('Error fetching cards info', error);
-
-    })
+//     })
  
-}
+// }
 
 
 // export async function fetchBySearch(page = 1) {
