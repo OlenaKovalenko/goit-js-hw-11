65c6d4a676f5cb2edfd0662efcc6f92249bd2dde
OlenaKovalenko <131033072+OlenaKovalenko@users.noreply.github.com>
2023-10-03 13:14:11 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -47,14 +47,14 @@ async function onFormSubmit(event) {
 
             simplelightbox .refresh();
                
-            const { height: cardHeight } = document
-              .querySelector(".gallery")
-              .firstElementChild.getBoundingClientRect();
+           //  const { height: cardHeight } = document
+           //    .querySelector(".gallery")
+           //    .firstElementChild.getBoundingClientRect();
 
-              window.scrollBy({
-              top: cardHeight * 2,
-              behavior: "smooth",
-           }); 
+           //    window.scrollBy({
+           //    top: cardHeight * 2,
+           //    behavior: "smooth",
+           // }); 
 
       
 
