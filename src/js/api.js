31e11b08400d8e5e105185a2b03a3114cde459d3
OlenaKovalenko <131033072+OlenaKovalenko@users.noreply.github.com>
2023-10-03 14:49:50 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -6,8 +6,8 @@ export const perPage = 40;
 
 export async function fetchBySearch(searchWord, page = 1) {
     try {
-    const API_KEY = '39708192-1d0c61ff60ff411770af0a0fc';
-    const BASE_URL = 'https://pixabay.111com/api/';
+    const API_KEY = '39708192-1d0c61ff60ff411770af0a0fc5555';
+    const BASE_URL = 'https://pixabay.com/123api/';
             
     const searchParams = new URLSearchParams({
         key: API_KEY,
